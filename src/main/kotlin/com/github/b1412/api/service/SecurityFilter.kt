6b4bb82c3e4991d6e0d805393ad3ee08f24c4a2f
leon.zhou <zhouleib1412@gmail.com>
2020-11-07 23:18:09 +1300

    update securityFilter interface

--- src/main/kotlin/com/github/b1412/api/service/SecurityFilter.kt
+++ src/main/kotlin/com/github/b1412/api/service/SecurityFilter.kt
@@ -1,12 +1,5 @@
 package com.github.b1412.api.service
 
-import org.springframework.security.core.userdetails.User
-
-
 interface SecurityFilter {
-
-    fun currentUser(): User
-
     fun query(method: String, requestURI: String): Map<String, String>
-
 }
