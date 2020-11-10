cc5004e924bfb46334ac14a566307d54b13f7723
leon.zhou <zhouleib1412@gmail.com>
2020-11-11 10:40:59 +1300

    rename convertors

--- src/main/kotlin/com/github/b1412/excel/convertor/LongConvertor.kt
+++ src/main/kotlin/com/github/b1412/excel/convertor/LongConvertor.kt
@@ -3,7 +3,7 @@ package com.github.b1412.excel.convertor
 import com.github.b1412.files.convert.CellConverter
 
 
-class LongConventor : CellConverter{
+class LongConvertor : CellConverter{
     override fun convert(value: String, obj: Any): Any {
         if (value.isEmpty()) {
             return 0L
